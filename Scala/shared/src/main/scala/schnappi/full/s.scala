95a6e51558f219dc4ae905ba66565b225561a852
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2021-08-16 06:12:31 +0000

    up

--- Scala/shared/src/main/scala/schnappi/full/s.scala
+++ Scala/shared/src/main/scala/schnappi/full/s.scala
@@ -1,5 +1,28 @@
 package schnappi.full
+import schnappi.core
+import schnappi.core.Err
+import schnappi.core.Identifier
+import schnappi.core.UniqueIdentifier
+import scala.collection.immutable.HashMap
 
-private object core {
-  import schnappi.core._
+sealed trait Exp {
+
 }
+
+final case class Context() // todo
+
+type UnifySubstitution = HashMap[Hole, Core]
+
+type State = UnifySubstitution
+
+final case class ElabOk[T](st: State, x: T)
+
+type Elab[T] = State=> Option[ElabOk[T]]
+
+sealed trait Core {
+  def check(context: Context, t: Core): Elab[core.Core] = ???
+  def infer(context:Context): Elab[(Core, core.Core)] = ???
+  def unify(other: Core): Option[UnifySubstitution] = ???
+  def walk: Elab[Core] = ???
+}
+final case class Hole(id: Identifier, uid: UniqueIdentifier) extends Core
