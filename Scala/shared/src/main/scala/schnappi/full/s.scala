adf5f9fd5d498f0a3518df3ef1bd8a42d73c5ed6
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2021-08-16 06:37:23 +0000

    up

--- Scala/shared/src/main/scala/schnappi/full/s.scala
+++ Scala/shared/src/main/scala/schnappi/full/s.scala
@@ -1,4 +1,5 @@
 package schnappi.full
+
 import schnappi.core
 import schnappi.core.Identifier
 import schnappi.core.UniqueIdentifier
@@ -6,7 +7,8 @@ import scala.collection.immutable.HashMap
 import scala.collection.immutable.LazyList
 
 sealed abstract class Err(x: String) extends Exception(x)
-final case class CoreErr(x:core.Err) extends Err(s"core err $x")
+
+final case class CoreErr(x: core.Err) extends Err(s"core err $x")
 
 sealed trait Exp {
 
@@ -20,21 +22,29 @@ type State = UnifySubstitution
 
 final case class ElabOk[T](st: State, x: T)
 
-type Elab[T] = State=> LazyList[ElabOk[T]|Err]
-implicit class ElabOps[T](x:Elab[T]) {
-  def flatMap[U](f: T=>Elab[U]): Elab[U] = state=>x(state).flatMap({
+type Elab[T] = State => LazyList[ElabOk[T] | Err]
+private def mplus[T](x: LazyList[T], y: LazyList[T]): LazyList[T] = if (x.isEmpty) y else x.head #:: mplus(y, x.tail)
+private def fairFlatten[T](x: LazyList[LazyList[T]]): LazyList[T] = if (x.isEmpty) LazyList() else mplus(x.head, fairFlatten(x.tail))
+
+implicit class ElabOps[T](x: Elab[T]) {
+  def flatMap[U](f: T => Elab[U]): Elab[U] = state => fairFlatten(x(state).map({
     case ElabOk(st, x) => f(x)(st)
-    case e:Err=>LazyList(e)
-  })
+    case e: Err => LazyList(e)
+  }))
 }
+
 object Elab {
-  def pure[T](x:T):Elab[T] = st=>LazyList(ElabOk(st,x))
+  def pure[T](x: T): Elab[T] = st => LazyList(ElabOk(st, x))
 }
 
 sealed trait Core {
   def check(context: Context, t: Core): Elab[core.Core] = ???
-  def infer(context:Context): Elab[(Core, core.Core)] = ???
+
+  def infer(context: Context): Elab[(Core, core.Core)] = ???
+
   def unify(other: Core): Option[UnifySubstitution] = ???
+
   def walk: Elab[Core] = ???
 }
+
 final case class Hole(id: Identifier, uid: UniqueIdentifier) extends Core
